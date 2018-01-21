dd5dc9c865a9a3774d384012c21aba0e99531cd3
jpaneva <jpaneva@gmail.com>
2018-01-21 13:01:59 +0200

    Add min constraint for max guests

--- application/view/host/index.php
+++ application/view/host/index.php
@@ -27,7 +27,7 @@
         </div>
         <div class="form-group">
             <label for="max-guests-input">Slots count</label>
-            <input type="number" class="form-control" id="max-guests-input" name="slots" required placeholder="Enter maximum number of attenders you want to have">
+            <input type="number" class="form-control" min="0" id="max-guests-input" name="slots" required placeholder="Enter maximum number of attenders you want to have">
         </div>
         <div class="form-group">
             <label for="desc-input">Additional info</label>
